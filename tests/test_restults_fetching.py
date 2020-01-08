61867d9bb5ef765821c172db8ccecb5cf0a3286b
voightp <vojtechpanek@seznam.cz>
2020-01-08 16:31:21 +0000

    added new test cases

--- tests/test_restults_fetching.py
+++ tests/test_restults_fetching.py
@@ -1,18 +1,39 @@
 import unittest
 from esofile_reader import EsoFile, DiffFile, TotalsFile, Variable
+from esofile_reader.constants import *
 
 
 class MyTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # cls.ef = EsoFile("../tests/eso_files/eplusout.eso", ignore_peaks=True)
-        # cls.ef_peaks = EsoFile("../tests/eso_files/eplusout.eso", ignore_peaks=False)
-        cls.ef = EsoFile("../tests/eso_files/eplusout1.eso")
+        cls.ef = EsoFile("../tests/eso_files/eplusout_all_intervals.eso")
 
-    def test_standard_results(self):
-        v1 = Variable("hourly", None, None, None)
-        print(self.ef.get_results(v1, include_day=True))
+    def test_basic_standard_results(self):
+        v = Variable(None, None, None, None)
+        r = self.ef.get_results(v)
+        self.assertEqual(r.shape, (17521, 114))
+        self.assertEqual(r.columns.names, ['key', 'variable', 'units'])
+        self.assertEqual(r.index.names, ["file", "timestamp"])
+
+        intervals = [TS, H, D, M, A, RP]
+        shapes = [(17520, 9), (8760, 9), (365, 9), (12, 9), (1, 9), (1, 9)]
+
+        for interval, shape in zip(intervals, shapes):
+            variables = [
+                Variable(interval, "Environment", "Site Diffuse Solar Radiation Rate per Area", "W/m2"),
+                Variable(interval, "BLOCK1:ZONE1", "Zone People Occupant Count", ""),
+                Variable(interval, "BLOCK1:ZONE1", "Zone Mean Air Temperature", "C"),
+                Variable(interval, "BLOCK2:ZONE1", "Zone Mean Air Temperature", "C"),
+                Variable(interval, "BLOCK3:ZONE1", "Zone Mean Air Temperature", "C"),
+                Variable(interval, "BLOCK4:ZONE1", "Zone Mean Air Temperature", "C"),
+                Variable(interval, "BLOCK1:ZONE1", "Zone Air Relative Humidity", "%"),
+                Variable(interval, "CHILLER", "Chiller Electric Power", "W"),
+                Variable(interval, "CHILLER", "Chiller Electric Energy", "J"),
+                Variable(interval, "non", "existing", "variable")
+            ]
+            r = self.ef.get_results(variables)
+            self.assertEqual(r.shape, shape)
 
 
-if __name__ == '__main__':
-    unittest.main()
+        if __name__ == '__main__':
+            unittest.main()
