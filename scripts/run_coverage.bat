0655c8efe5bf7f8049568297ec1889889d555e16
voightp <vojtechpanek@seznam.cz>
2020-01-29 14:35:58 +0000

    added coverage report and html command

--- scripts/run_coverage.bat
+++ scripts/run_coverage.bat
@@ -1,1 +1,2 @@
-cd ..\venv\scripts & activate & cd ..\.. & coverage run --source=esofile_reader -m unittest discover -s ./tests -t ./tests -p test_*.py
+cd ..\venv\scripts & activate & cd ..\.. & coverage run --source=esofile_reader -m unittest discover -s ./tests -t ./tests -p test_*.py^
+ & coverage report --omit esofile_reader\outputs\base_data.py & coverage html --omit esofile_reader\outputs\base_data.py
