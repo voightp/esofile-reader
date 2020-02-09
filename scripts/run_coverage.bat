daf94607cab14da00fe2c040f42398b79e9147e6
voightp <vojtechpanek@seznam.cz>
2020-02-09 17:08:38 +0000

    updated coverage settings

--- scripts/run_coverage.bat
+++ scripts/run_coverage.bat
@@ -1,2 +1,2 @@
-cd ..\venv\scripts & activate & cd ..\.. & coverage run --source=esofile_reader -m unittest discover -s ./tests -t ./tests -p test_*.py^
+cd ..\venv\scripts & activate & cd ..\.. & coverage run -m unittest discover -s ./tests -t ./tests -p test_*.py^
  & coverage report  & coverage html
