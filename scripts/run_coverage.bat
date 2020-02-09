a038c41f33fda8e5e651d1bbd942ae309e8aaadd
voightp <vojtechpanek@seznam.cz>
2020-02-09 15:11:50 +0000

    new coverage config file

--- scripts/run_coverage.bat
+++ scripts/run_coverage.bat
@@ -1,2 +1,2 @@
 cd ..\venv\scripts & activate & cd ..\.. & coverage run --source=esofile_reader -m unittest discover -s ./tests -t ./tests -p test_*.py^
- & coverage report --omit esofile_reader\storage\base_storage.py & coverage html --omit esofile_reader\storage\base_storage.py
+ & coverage report  & coverage html
