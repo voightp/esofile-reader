86490fd778a8ea10b4d0ea51a8118291feb759bb
voightp <vojtechpanek@seznam.cz>
2020-06-22 08:14:41 +0100

    SimpleVariable included

--- esofile_reader/__init__.py
+++ esofile_reader/__init__.py
@@ -1,5 +1,5 @@
 from esofile_reader.eso_file import EsoFile
 from esofile_reader.fetchers import get_results
-from esofile_reader.mini_classes import Variable
+from esofile_reader.mini_classes import Variable, SimpleVariable
 from esofile_reader.results_file import ResultsFile
 from esofile_reader.storage.sql_storage import SQLFile
