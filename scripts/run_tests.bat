312f7fada15ee3e40a2f85f9ffa97dca65e6fb57
voightp <vojtechpanek@seznam.cz>
2020-07-02 11:05:34 +0100

    update script with new venv, exclude ci tests when updating script folder

--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,1 +1,1 @@
-cd ..\venv\scripts & activate & cd ..\.. & python -m unittest discover -s ./tests -t ./tests -p test_*.py
+cd ..\.venv\scripts & activate & cd ..\.. & python -m unittest discover -s ./tests -t ./tests -p test_*.py
