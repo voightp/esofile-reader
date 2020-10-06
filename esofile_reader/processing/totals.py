0d7fe12d324029705d06db4e87b5c619abd64530
voightp <vojtechpanek@seznam.cz>
2020-10-06 13:54:51 +0100

    filter non numeric types when generating totals

--- esofile_reader/processing/totals.py
+++ esofile_reader/processing/totals.py
@@ -1,8 +1,9 @@
 import logging
 import re
-from typing import Dict, Generator, Optional
+from typing import Dict, Generator, Optional, Tuple
 
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
 
 from esofile_reader.constants import *
 from esofile_reader.id_generator import incremental_id_gen
@@ -101,6 +102,30 @@ def calculate_totals(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def get_grouped_variable(key: str, type_: str, units: str) -> Tuple[str, str, str]:
+    """ Create a new key and type for grouped variable. """
+    group_keyword = type_
+    subgroup_keyword = _get_keyword(key, SUBGROUPS)
+    if subgroup_keyword:
+        group_keyword = f"{subgroup_keyword} {group_keyword}"
+        key = subgroup_keyword  # assign a new key based on subgroup keyword
+    elif key == "Cumulative Meter" or key == "Meter":
+        if "#" in type_:
+            # use last substring as a key
+            group_keyword = type_.split("#")[-1]
+            type_ = group_keyword
+    else:
+        # assign key based on 'Variable' category
+        # the category is missing, use a first word in 'type' string
+        key = _get_group_key(type_, VARIABLE_GROUPS)
+        if not key:
+            key = type_.split(sep=" ", maxsplit=1)[0]
+    # there could be a potential issue with variables which
+    # would have the same type but different units
+    group_keyword = f"{group_keyword} {units}"
+    return key, type_, group_keyword
+
+
 def create_grouped_multiindex(
     id_gen: Generator[int, None, None], columns: pd.MultiIndex
 ) -> pd.MultiIndex:
@@ -108,58 +133,39 @@ def create_grouped_multiindex(
     groups = {}
     muiltiindex_items = []
     for id_, table, key, type_, units in columns:
-
         # variable can be grouped only if it's included as avg or sum
         group = (units in SUMMED_UNITS or units in AVERAGED_UNITS) and key != SPECIAL
         if group:
             # initialize group string to be the same as type
-            gr_str = type_
-            w = _get_keyword(key, SUBGROUPS)
-            if key == "Cumulative Meter" or key == "Meter":
-                if "#" in type_:
-                    # use last substring as a key
-                    gr_str = type_.split("#")[-1]
-                    type_ = gr_str
-            elif w:
-                gr_str = w + " " + gr_str
-                key = w  # assign a new key based on subgroup keyword
-            else:
-                # assign key based on 'Variable' category
-                # the category is missing, use a first word in 'Variable' string
-                key = _get_group_key(type_, VARIABLE_GROUPS)
-                if not key:
-                    key = type_.split(sep=" ", maxsplit=1)[0]
-
-            if gr_str in groups:
+            key, type_, group_keyword = get_grouped_variable(key, type_, units)
+            if group_keyword in groups:
                 # variable group already exist, get id of the existing group
-                group_id = groups[gr_str]
+                group_id = groups[group_keyword]
             else:
                 # variable group does not exist yet, create new group id
                 # and store group reference for consequent variables
                 group_id = next(id_gen)
-                groups[gr_str] = group_id
+                groups[group_keyword] = group_id
         else:
             # units cannot be grouped, create an independent variable
             group_id = next(id_gen)
-
         muiltiindex_items.append((group_id, table, key, type_, units))
-
     return pd.MultiIndex.from_tuples(muiltiindex_items, names=COLUMN_LEVELS)
 
 
 def filter_invalid_variables(df: pd.DataFrame):
-    srs = []
-    types = df.columns.get_level_values(TYPE_LEVEL)
-
     # ignored type may be a substring to catch types such as
     # 'Performance Curve Input Variable 1', 'Performance Curve Input Variable 2'
+    srs = []
+    types = df.columns.get_level_values(TYPE_LEVEL)
     for ignored_type in IGNORED_TYPES:
         srs.append(types.str.contains(ignored_type))
 
     cond1 = ~pd.DataFrame(srs).apply(lambda x: x.any()).to_numpy()
     cond2 = ~df.columns.get_level_values(UNITS_LEVEL).isin(IGNORED_UNITS)
+    cond3 = df.apply(is_numeric_dtype)
 
-    return df.loc[:, cond1 & cond2]
+    return df.loc[:, cond1 & cond2 & cond3]
 
 
 def process_totals_table(
