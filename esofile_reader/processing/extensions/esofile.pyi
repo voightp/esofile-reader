c04da0a79bd285c84d86b98f03e090a986f65e6f
voightp <vojtechpanek@seznam.cz>
2020-11-12 12:25:24 +0000

    apply black

--- esofile_reader/processing/extensions/esofile.pyi
+++ esofile_reader/processing/extensions/esofile.pyi
@@ -6,58 +6,31 @@ from esofile_reader.mini_classes import Variable, EsoTimestamp
 from esofile_reader.processing.progress_logger import GenericLogger
 from esofile_reader.processing.raw_data import RawEsoData
 
-
 def get_eso_file_version(raw_version: str) -> int: ...
-
-
 def get_eso_file_timestamp(timestamp: str) -> datetime: ...
-
-
 def process_statement_line(line: str) -> Tuple[int, datetime]: ...
-
-
 def process_header_line(line: str) -> Tuple[int, str, str, str, str]: ...
-
-
 def read_header(
     eso_file: TextIO, progress_logger: GenericLogger
 ) -> Dict[str, Dict[int, Variable]]: ...
-
-
 def process_sub_monthly_interval_line(
     line_id: int, data: List[str]
 ) -> Tuple[str, EsoTimestamp, str]: ...
-
-
 def process_monthly_plus_interval_line(
     line_id: int, data: List[str]
 ) -> Tuple[str, EsoTimestamp, Optional[int]]: ...
-
-
 def read_body(
     eso_file: TextIO,
     highest_interval_id: int,
     header: Dict[str, Dict[int, Variable]],
     ignore_peaks: bool,
-    progress_logger: GenericLogger
+    progress_logger: GenericLogger,
 ) -> List[RawEsoData]: ...
-
-
 def read_file(
     file: TextIO, progress_logger: GenericLogger, ignore_peaks: bool = True
 ) -> List[RawEsoData]: ...
-
-
 def count_lines(file_path: Union[str, Path]) -> int: ...
-
-
-def preprocess_file(
-    file_path: Union[str, Path], progress_logger: GenericLogger
-) -> None: ...
-
-
+def preprocess_file(file_path: Union[str, Path], progress_logger: GenericLogger) -> None: ...
 def process_eso_file(
-    file_path: Union[str, Path],
-    progress_logger: GenericLogger,
-    ignore_peaks: bool = True,
+    file_path: Union[str, Path], progress_logger: GenericLogger, ignore_peaks: bool = True,
 ) -> List[RawEsoData]: ...
